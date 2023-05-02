3c385248afba000bda30b620a8ada6b85c7b113a
alxwaugh <alxwaugh@users.noreply.github.com>
2023-05-02 20:42:01 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -4,15 +4,15 @@
 const config = {
   style: 'mapbox://styles/mapbox/light-v10',
   accessToken:
-    'pk.eyJ1IjoiYWxleHdhdWdoIiwiYSI6ImNsaDZsbm9veTA3M2EzZG5zeTRjaW4zanEifQ.rKuL5W8QsXaaSMaWOPhimA',
-  CSV: './Meets.csv',
-  center: [0.00, 51.398],
+    'pk.eyJ1IjoibWFwYm94LWNvbW11bml0eSIsImEiOiJja2tkN21jcjAwMG51MnBxdHAxemdueGpzIn0.e0IzLkytGq4pcGGieP8KNA',
+  CSV: './Sample_Data.csv',
+  center: [-120.234, 47.398],
   zoom: 6,
-  title: 'Track Meet',
+  title: 'Replace with your title',
   description:
-    'Find track meets near you',
-  sideBarInfo: ['Event Name', 'Date', 'Venue'],
-  popupInfo: ['Event Name'],
+    'Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.',
+  sideBarInfo: ['Location_Name', 'Address', 'Phone'],
+  popupInfo: ['Location_Name'],
   filters: [
     {
       type: 'dropdown',
