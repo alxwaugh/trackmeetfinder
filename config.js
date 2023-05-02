5223ac10a83699742f07f3527f41802e6bf3efdd
alxwaugh <alxwaugh@users.noreply.github.com>
2023-05-02 20:42:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const config = {
   style: 'mapbox://styles/mapbox/light-v10',
   accessToken:
-    'pk.eyJ1IjoibWFwYm94LWNvbW11bml0eSIsImEiOiJja2tkN21jcjAwMG51MnBxdHAxemdueGpzIn0.e0IzLkytGq4pcGGieP8KNA',
+    'pk.eyJ1IjoiYWxleHdhdWdoIiwiYSI6ImNsaDZsbm9veTA3M2EzZG5zeTRjaW4zanEifQ.rKuL5W8QsXaaSMaWOPhimA',
   CSV: './Sample_Data.csv',
   center: [-120.234, 47.398],
   zoom: 6,
