01914ef3776782df0133d987806d74fe31e03cae
alxwaugh <alxwaugh@users.noreply.github.com>
2023-05-02 19:02:29 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const config = {
+  style: 'mapbox://styles/mapbox/light-v10',
+  accessToken:
+    'pk.eyJ1IjoiYWxleHdhdWdoIiwiYSI6ImNsaDZrcmhrcDA3N2Uza25za2w4ZWRrYWcifQ.VPFuUGN0AXWBIh7EhLpMBA',
+  CSV: './Meets.csv',
+  center: [0.00, 51.398],
+  zoom: 6,
+  title: 'Track Meet',
+  description:
+    'Find track meets near you',
+  sideBarInfo: ['Event_Name', 'Date', 'Venue'],
+  popupInfo: ['Event_Name'],
+  filters: [
+    {
+      type: 'dropdown',
+      title: 'Languages supported: ',
+      columnHeader: 'Languages',
+      listItems: [
+        'Amharic',
+        'ASL',
+        'Cambodian',
+        'Chinese',
+        'Danish',
+        'English',
+        'French',
+        'German',
+        'Greek',
+        'Hindi',
+        'Italian',
+        'Japanese',
+        'Korean',
+        'Language Line Services',
+        'Norwegian',
+        'Oriya',
+        'Portuguese',
+        'Punjabi',
+        'Russian',
+        'Somali',
+        'Spanish',
+        'Swedish',
+        'Tagalog',
+        'Thai',
+        'Tigrinya',
+        'Tongan',
+        'Vietnamese',
+        'Ukranian',
+      ],
+    },
+    {
+      type: 'checkbox',
+      title: 'Devices available: ',
+      columnHeader: 'Devices_available', // Case sensitive - must match spreadsheet entry
+      listItems: ['Computer', 'Wi-Fi', 'Adaptive Laptops'], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
+    },
+    {
+      type: 'dropdown',
+      title: 'Clients: ',
+      columnHeader: 'Clients',
+      listItems: [
+        'Adults',
+        'Disabled',
+        'Homeless',
+        'Immigrants/Refugees',
+        'Low Income',
+        'Seniors',
+        'Youth: Pre-teen',
+        'Youth: Teen',
+      ],
+    },
+  ],
+};
